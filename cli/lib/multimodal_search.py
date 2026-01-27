1df1e07265dc577cced1ae72e19a2140ec0e9c5e
Florian Schuett <flo.schuett@chello.at>
2026-01-27 21:32:15 +0100

    Add image search

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -2,10 +2,19 @@ import os
 from PIL import Image
 from sentence_transformers import SentenceTransformer
 
+from lib.search_utils import Document
+from lib.semantic_search import cosine_similarity
+from lib.search_utils import load_movies
+
 
 class MultimodalSearch:
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(self, documents: list[Document] = [], model_name="clip-ViT-B-32"):
         self.model = SentenceTransformer(model_name)
+
+        self.documents = documents
+        self.texts = [f"{doc.title}: {doc.description}" for doc in self.documents]
+
+        self.text_embeddings = self.model.encode(self.texts, show_progress_bar=False)
 
     def embed_image(self, image_path: str):
         if not os.path.exists(image_path):
@@ -14,8 +23,25 @@ class MultimodalSearch:
         image_embedding = self.model.encode([image])  # ty:ignore[no-matching-overload]
         return image_embedding[0]
 
+    def search_with_image(self, image_path: str):
+        if not os.path.exists(image_path):
+            raise FileNotFoundError(f"Image file not found: {image_path}")
+
+        image_embedding = self.embed_image(image_path)
+        docs_with_scores = [{
+            "doc": self.documents[index],
+            "score": cosine_similarity(image_embedding, embedding),
+        } for index, embedding in enumerate(self.text_embeddings)]
+        sorted_results = sorted(docs_with_scores, key=lambda item: item["score"], reverse=True)
+        return sorted_results[:5]
+
 
 def verify_image_embedding(image_path: str):
     searcher = MultimodalSearch()
     embedding = searcher.embed_image(image_path)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
+
+def image_search_command(image_path: str):
+    movies = load_movies()
+    searcher = MultimodalSearch(movies)
+    return searcher.search_with_image(image_path)
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -1,6 +1,7 @@
 import argparse
 
-from lib.multimodal_search import verify_image_embedding
+from lib.multimodal_search import verify_image_embedding, image_search_command
+from lib.search_utils import DEFAULT_PEVIEW_LENGTH
 
 
 def main():
@@ -13,12 +14,24 @@ def main():
     verify_image_embedding_parser.add_argument(
         "image", type=str, help="Path to image file"
     )
+    image_search_parser = subparsers.add_parser(
+        "image_search", help="Search documents using an image"
+    )
+    image_search_parser.add_argument(
+        "image", type=str, help="Path to image file"
+    )
 
     args = parser.parse_args()
 
     match args.command:
         case "verify_image_embedding":
             verify_image_embedding(args.image)
+        case "image_search":
+            results = image_search_command(args.image)
+            for index, res in enumerate(results, 1):
+                doc = res["doc"]
+                score = res["score"]
+                print(f"{index}. {doc.title} (similarity: {score:.3f})\n\t{doc.description[:DEFAULT_PEVIEW_LENGTH]}...\n")
         case _:
             parser.print_help()
 
